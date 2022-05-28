462f8e3fa0ce2bb2365a740b578ed7570e0b704d
Mazy <landonp95@gmail.com>
2022-05-28 00:01:29 -0400

    more experiment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,6 @@
   <meta name="description" content="A svelte thingy majik!" />
 </svelte:head>
 
-<div class="text-green-500 mx-auto h-screen">
+<div class="text-blue-300 mx-auto h-screen bg-gradient-to-r from-blue-700 to-red-600">
   <MdAccessAlarms />
 </div>
